e1ce4dea5b16e940f96e384a1ff02589ff04e4de
yaok <yaok@pingansec.com>
2019-11-19 20:03:54 +0800

    修改服务端口为8888

--- models/dbresource/utndatanew/main.go
+++ models/dbresource/utndatanew/main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
-	"fmt"
 )
 
 func init()  {
@@ -19,7 +18,6 @@ func init()  {
 	}
 
 	dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport +")" + "/" + dbname + "?charset=utf8"
-	fmt.Println(dsn)
 	//orm.RegisterDriver("mysql",orm.DRMySQL)
 	orm.RegisterDataBase("default","mysql",dsn)
 }
