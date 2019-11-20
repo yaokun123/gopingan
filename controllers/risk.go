46601f35235f9772dd126e19e5d16fdfe23dd445
yaok <yaok@pingansec.com>
2019-11-20 17:46:18 +0800

    调整协程

--- controllers/risk.go
+++ controllers/risk.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"gopingan/models/mymongo"
 	"gopingan/models"
+	"github.com/astaxie/beego/orm"
+	"sync"
 )
 
 type RiskController struct {
@@ -15,7 +17,29 @@ func (this *RiskController) GetInfo()  {
 	digest := this.GetString("digest")
 	company_name := mymongo.GetMongoInfoByDigest(digest)
 
-	//开庭公告
+	//创建waitGroup
+	wg := sync.WaitGroup{}
+
+
+	//1、开庭公告
+	courtNoticeResultChannel := make(chan []orm.Params)
+	courtNoticeMapCountChannel := make(chan int64)
+	wg.Add(1)
+	go getCourtNotice(company_name,courtNoticeResultChannel,courtNoticeMapCountChannel,&wg)
+
+
+	wg.Wait()
+	courtNoticeResult := <- courtNoticeResultChannel
+	courtNoticeMapCount := <- courtNoticeMapCountChannel
+	fmt.Println(courtNoticeMapCount)
+	fmt.Println(courtNoticeResult)
+	this.Data["json"] = courtNoticeResult
+	this.ServeJSON()
+}
+
+
+//开庭公告
+func getCourtNotice(company_name string,courtNoticeResultChannel chan []orm.Params,courtNoticeMapCountChannel chan int64,wg *sync.WaitGroup) {
 	courtNoticeMapResult,courtNoticeMapCount := models.GetCourtNoticesMapInfoByName(company_name,0)
 	var idList []int
 	for _,item := range courtNoticeMapResult{
@@ -23,8 +47,7 @@ func (this *RiskController) GetInfo()  {
 	}
 	courtNoticeResult := models.GetCourtNoticesInfoByIds(idList)
 
-	fmt.Println(courtNoticeMapCount)
-	fmt.Println(courtNoticeResult)
-	this.Data["json"] = courtNoticeResult
-	this.ServeJSON()
+	courtNoticeResultChannel <- courtNoticeResult
+	courtNoticeMapCountChannel <- courtNoticeMapCount
+	wg.Done()
 }
