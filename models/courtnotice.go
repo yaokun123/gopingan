35f7a9a6fea64e9f5029f150f61010a5a506e675
yaok <yaok@pingansec.com>
2019-11-20 11:21:46 +0800

    sql debug

--- models/courtnotice.go
+++ models/courtnotice.go
@@ -20,9 +20,15 @@ func GetCourtNoticesInfoByIds(ids []int)  {
 	O.Raw(sql,ids).Values(&maps)
 
 	for index,item := range maps {
-		maps[index]["startDate"] = item["startDate"].(string)[0:10]
-		maps[index]["update_date"] = item["update_date"].(string)[0:10]
-		maps[index]["create_time"] = item["create_time"].(string)[0:10]
+		if item["startDate"] != nil{
+			maps[index]["startDate"] = item["startDate"].(string)[0:10]
+		}
+		if item["update_date"] != nil{
+			maps[index]["update_date"] = item["update_date"].(string)[0:10]
+		}
+		if item["create_time"] != nil{
+			maps[index]["create_time"] = item["create_time"].(string)[0:10]
+		}
 	}
 
 	fmt.Println(maps)
