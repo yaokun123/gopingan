17a024dbc706144b0c60cb75531d53bacdfa82be
yaok <yaok@pingansec.com>
2019-11-20 13:15:32 +0800

    sql debug

--- models/courtnotice.go
+++ models/courtnotice.go
@@ -23,6 +23,8 @@ func GetCourtNoticesInfoByIds(ids []int)  {
 	//公司名检测plaintiff/defendant
 	var company_name_list []string
 	for index,item := range maps {
+
+		//截取时间
 		if item["startDate"] != nil{
 			maps[index]["startDate"] = item["startDate"].(string)[0:10]
 		}
@@ -32,44 +34,52 @@ func GetCourtNoticesInfoByIds(ids []int)  {
 		if item["create_time"] != nil{
 			maps[index]["create_time"] = item["create_time"].(string)[0:10]
 		}
-		plaintiffs := strings.Split(item["plaintiff"].(string),",")
-		for index2,company_name := range plaintiffs{
-			if utf8.RuneCountInString(company_name) > 4{//小于4个字的不取公司名
-				company_name_list = append(company_name_list,company_name)
-			}else if utf8.RuneCountInString(company_name) == 3{
-				tmp := []rune(company_name)
-				first_char := string(tmp[0:1])
-				end_char := string(tmp[1:1])
-				result_name := first_char + "*" + end_char
-				plaintiffs[index2] = result_name
-			}else if utf8.RuneCountInString(company_name) == 2{
-				tmp := []rune(company_name)
-				first_char := string(tmp[0:1])
-				result_name := first_char + "*"
-				plaintiffs[index2] = result_name
-			}
-		}
-		maps[index]["plaintiff"] = plaintiffs
 
-		defendants := strings.Split(item["defendant"].(string),",")
-		fmt.Println(defendants)
-		/*for index2,company_name := range defendants{
-			if utf8.RuneCountInString(company_name) > 4{//小于4个字的不取公司名
-				company_name_list = append(company_name_list,company_name)
-			}else if utf8.RuneCountInString(company_name) == 3{
-				tmp := []rune(company_name)
-				first_char := string(tmp[0:1])
-				end_char := string(tmp[1:1])
-				result_name := first_char + "*" + end_char
-				plaintiffs[index2] = result_name
-			}else if utf8.RuneCountInString(company_name) == 2{
-				tmp := []rune(company_name)
-				first_char := string(tmp[0:1])
-				result_name := first_char + "*"
-				plaintiffs[index2] = result_name
+		//处理公诉人/原告/上诉人/申请人
+		if item["plaintiff"] != nil{
+			plaintiffs := strings.Split(item["plaintiff"].(string),",")
+			for index2,company_name := range plaintiffs{
+				if utf8.RuneCountInString(company_name) > 4{//小于4个字的不取公司名
+					company_name_list = append(company_name_list,company_name)
+				}else if utf8.RuneCountInString(company_name) == 3{
+					tmp := []rune(company_name)
+					first_char := string(tmp[0:1])
+					end_char := string(tmp[1:1])
+					result_name := first_char + "*" + end_char
+					plaintiffs[index2] = result_name
+				}else if utf8.RuneCountInString(company_name) == 2{
+					tmp := []rune(company_name)
+					first_char := string(tmp[0:1])
+					result_name := first_char + "*"
+					plaintiffs[index2] = result_name
+				}
 			}
+			maps[index]["plaintiff"] = plaintiffs
 		}
-		maps[index]["defendant"] = defendants*/
+
+		//处理被告人/被告/被上诉人/被申请人
+		if item["defendant"] != nil{
+			defendants := strings.Split(item["defendant"].(string),",")
+			fmt.Println(defendants)
+			for index2,company_name := range defendants{
+				if utf8.RuneCountInString(company_name) > 4{//小于4个字的不取公司名
+					company_name_list = append(company_name_list,company_name)
+				}else if utf8.RuneCountInString(company_name) == 3{
+					tmp := []rune(company_name)
+					first_char := string(tmp[0:1])
+					end_char := string(tmp[1:1])
+					result_name := first_char + "*" + end_char
+					defendants[index2] = result_name
+				}else if utf8.RuneCountInString(company_name) == 2{
+					tmp := []rune(company_name)
+					first_char := string(tmp[0:1])
+					result_name := first_char + "*"
+					defendants[index2] = result_name
+				}
+			}
+			maps[index]["defendant"] = defendants
+		}
+
 	}
 
 	fmt.Println(maps)
