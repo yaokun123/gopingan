f7b5e955eb49b932f5d3fe091568ff8a57d8b129
yaok <yaok@pingansec.com>
2019-11-20 10:46:23 +0800

    sql debug

--- models/courtnotice.go
+++ models/courtnotice.go
@@ -2,14 +2,17 @@ package models
 
 import (
 	"strings"
-	"fmt"
 )
 
 func GetCourtNoticesInfoByIds(ids []int)  {
 	length := len(ids)
 	where := strings.Repeat("?,",length)
 	where = strings.TrimRight(where,",")
-	fmt.Println(where)
+	where = "("+where+")"
+
+	sql := "select * form court_notices where id in "+where
+	list,_ := O.Raw(sql,ids).Exec()
+	print(list)
 }
 
 
